c3842d407fa2b8a9cacaf55f831a8fbae15750cc
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-10-07 21:20:52 +0200

    Update Main.java

--- src/ex_2/Main.java
+++ src/ex_2/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 7; i++) {
 			Coche coche = new Coche(i);
+			System.out.print(coche.toStringWithId() + " ");
 			new Thread(coche).start();
 		}
 		System.out.println("\n");
